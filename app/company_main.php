3ea984e2a86f21153ba7450f32fcd7e61f22528e
sheway <67420772+sheway@users.noreply.github.com>
2021-12-27 16:27:04 +0800

    connect sql
    connect sql

--- app/company_main.php
+++ app/company_main.php
@@ -1,3 +1,15 @@
+<?
+header("Connect-Type: text/html; charset = utf-8");
+include("connectMysql.php");
+$Company_id = 'C0001';
+$sql_query = "SELECT * FROM company WHERE Company_id={$Company_id}";
+$Company_info = $db_link->query($sql_query);
+
+$sql_query = "SELECT * FROM contract WHERE Company_id={$Company_id}";
+$All_Contract_info = $db_link->query($sql_query);
+
+$db_link->close();
+?>
 ---
 title: Boostrap Index
 layout: ./app/layout.ejs
@@ -16,7 +28,7 @@ current: index
     <ul class="list-group">
         <li class="list-group-item">
             <div class="d-flex align-items-center">
-                合約名稱
+                合約1
                 <span class="badge rounded-pill bg-success ms-4">第一級</span>
                 <a class="btn btn-primary ms-auto" href="#" role="button">檢視合約資料</a>
                 <button type="button" class="btn btn-primary ms-4">簽署新合約</button>
@@ -30,8 +42,8 @@ current: index
         </li>
         <li class="list-group-item">
             <div class="d-flex align-items-center">
-                合約名稱
-                    <span class="badge rounded-pill bg-success ms-4">第一級</span>
+                合約2
+                    <span class="badge rounded-pill bg-success ms-4">第二級</span>
                 <a class="btn btn-primary ms-auto" href="#" role="button">檢視合約資料</a>
                 <button type="button" class="btn btn-primary ms-4">簽署新合約</button>
                 <button type="button" class="btn btn-outline-primary fw-bold px-2 ms-4" data-bs-toggle="collapse" data-bs-target="#contract-2" aria-expanded="false" aria-controls="contract-2">
@@ -39,20 +51,6 @@ current: index
                 </button>
             </div>
             <div class="collapse" id="contract-2">
-                  <p class="mt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi dolorem quisquam porro totam labore omnis ipsa quia corporis unde ducimus deleniti repellat a vitae distinctio alias qui optio, dicta repellendus fuga! Eos quos voluptatum ipsum sunt, reiciendis veritatis sequi incidunt iusto adipisci similique totam vel exercitationem magnam maxime, quasi enim?</p>
-              </div>
-        </li>
-        <li class="list-group-item">
-            <div class="d-flex align-items-center">
-                合約名稱
-                    <span class="badge rounded-pill bg-success ms-4">第一級</span>
-                <a class="btn btn-primary ms-auto" href="#" role="button">檢視合約資料</a>
-                <button type="button" class="btn btn-primary ms-4">簽署新合約</button>
-                <button type="button" class="btn btn-outline-primary fw-bold px-2 ms-4" data-bs-toggle="collapse" data-bs-target="#contract-3" aria-expanded="false" aria-controls="contract-3">
-                    <span class="material-icons align-middle">expand_more</span>
-                </button>
-            </div>
-            <div class="collapse" id="contract-3">
                   <p class="mt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi dolorem quisquam porro totam labore omnis ipsa quia corporis unde ducimus deleniti repellat a vitae distinctio alias qui optio, dicta repellendus fuga! Eos quos voluptatum ipsum sunt, reiciendis veritatis sequi incidunt iusto adipisci similique totam vel exercitationem magnam maxime, quasi enim?</p>
               </div>
         </li>
